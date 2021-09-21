4874a2f529dc020333da5dae60b18bae49012d5c
KrEdu <kristians12x@gmail.com>
2021-09-21 20:52:37 +0300

    Imported Models

---
+++ Models/AirportValidate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlightPlannerAPI.Models
+{
+    public static class AirportValidate
+    {
+        public static bool AreAirportsViable(Airport airport1, Airport airport2)
+        {
+            if (airport1 == null || airport2 == null ||
+                String.IsNullOrEmpty(airport1.airport) || String.IsNullOrEmpty(airport2.airport) ||
+               String.IsNullOrEmpty(airport1.airport) || String.IsNullOrEmpty(airport2.airport) ||
+                String.IsNullOrEmpty(airport1.airport) || String.IsNullOrEmpty(airport2.airport))
+            {
+                return false;
+            }
+            else if (airport1.airport.ToLower().Trim() == airport2.airport.ToLower().Trim())
+            {
+                return false;
+            }
+            else return true;
+        }
+
+        public static bool AreAirportsDuplicate(Airport airport1, Airport airport2)
+        {
+            return airport1.airport.ToLower().Trim() == airport2.airport.ToLower().Trim();
+        }
+    }
+}
---
+++ Models/FlightValidate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlightPlannerAPI.Models
+{
+    public static class FlightValidate
+    {
+        public static bool IsFlightViable(Flight flight)
+        {
+            if (!AirportValidate.AreAirportsViable(flight.From,flight.To) ||
+               String.IsNullOrEmpty(flight.Carrier) || String.IsNullOrEmpty(flight.DepartureTime) || String.IsNullOrEmpty(flight.ArrivalTime))
+            {
+                return false;
+            }
+            else if (DateTime.Parse(flight.DepartureTime) > DateTime.Parse(flight.ArrivalTime) ||
+                DateTime.Parse(flight.DepartureTime) == DateTime.Parse(flight.ArrivalTime))
+            {
+                return false;
+            }
+            else return true;
+        }
+
+        public static bool AreFlightDuplicates(Flight flight1, Flight flight2)
+        {
+            return flight1.DepartureTime == flight2.DepartureTime &&
+                flight1.ArrivalTime == flight2.ArrivalTime
+                && flight1.Carrier == flight2.Carrier
+                && AirportValidate.AreAirportsDuplicate(flight1.To, flight2.To)
+                && AirportValidate.AreAirportsDuplicate(flight1.From, flight2.From);
+        }
+    }
+}
