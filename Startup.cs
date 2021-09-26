aa6dfbb2e0a0a9215d6b3dce2c65d0433aaad568
KrEdu <kristians12x@gmail.com>
2021-09-26 17:22:19 +0300

    Cleaned up code and added dbContext services to Startup

--- Startup.cs
+++ Startup.cs
@@ -1,17 +1,13 @@
 using FlightPlannerAPI.Authentification;
+using FlightPlannerAPI.DbContext;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace FlightPlannerAPI
 {
@@ -27,6 +23,8 @@ namespace FlightPlannerAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<FlightPlannerDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("flight-planner")));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
