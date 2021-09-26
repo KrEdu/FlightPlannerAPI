48d5951ad6e4a6efb01049d5d45408125127033c
KrEdu <kristians12x@gmail.com>
2021-09-26 17:19:58 +0300

    Added dbContext

---
+++ DbContext/FlightPlannerDbContext.cs
@@ -0,0 +1,16 @@
+﻿using FlightPlannerAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace FlightPlannerAPI.DbContext
+{
+    public class FlightPlannerDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public FlightPlannerDbContext(DbContextOptions<FlightPlannerDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Airport> Airports { get; set; }
+        public DbSet<Flight> Flights { get; set; }
+    }
+}
